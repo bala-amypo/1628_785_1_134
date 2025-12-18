9abf880359342984ccb19f5d4cb6bafcd69cd778
bala-amypo <bala@amypo.in>
2025-12-18 18:48:58 +0000

    User commit

--- src/main/java/com/example/demo/repository/PatientProfileRepository.java
+++ src/main/java/com/example/demo/repository/PatientProfileRepository.java
@@ -1,1 +1,14 @@
 package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.PatientProfile;
+
+
+    @Repository
+public interface  PatientProfileRepository extends JpaRepository <PatientProfile ,Long>{
+  
+}
+
+
