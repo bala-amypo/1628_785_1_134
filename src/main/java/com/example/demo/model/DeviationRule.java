2966ecd75a71e76375d2fb2e7694672a1ba17cbf
bala-amypo <bala@amypo.in>
2025-12-18 18:33:58 +0000

    User commit

--- src/main/java/com/example/demo/model/DeviationRule.java
+++ src/main/java/com/example/demo/model/DeviationRule.java
@@ -1,1 +1,55 @@
-package com.example.demo.DeviationRule;
+package com.example.demo.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class DeviationRule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String surgeryType;
+    private String symptomParameter;
+    private int thresholdDevation;
+    private Boolean active;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getSurgeryType() {
+        return surgeryType;
+    }
+    public void setSurgeryType(String surgeryType) {
+        this.surgeryType = surgeryType;
+    }
+    public String getSymptomParameter() {
+        return symptomParameter;
+    }
+    public void setSymptomParameter(String symptomParameter) {
+        this.symptomParameter = symptomParameter;
+    }
+    public int getThresholdDevation() {
+        return thresholdDevation;
+    }
+    public void setThresholdDevation(int thresholdDevation) {
+        this.thresholdDevation = thresholdDevation;
+    }
+    public Boolean getActive() {
+        return active;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+    public DeviationRule(Long id, String surgeryType, String symptomParameter, int thresholdDevation, Boolean active) {
+        this.id = id;
+        this.surgeryType = surgeryType;
+        this.symptomParameter = symptomParameter;
+        this.thresholdDevation = thresholdDevation;
+        this.active = active;
+    }
+    public DeviationRule() {
+    }
+
+}
