9ae3dc2d11c412514aebbe6798d502fcc8a8295c
bala-amypo <bala@amypo.in>
2025-12-18 18:01:12 +0000

    User commit

--- src/main/java/com/example/demo/model/PatientProfile.java
+++ src/main/java/com/example/demo/model/PatientProfile.java
@@ -1,13 +1,85 @@
-package com.example.demo.PatientProfile;
+package com.example.demo.model;
 
-public class PatientProfile{
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class PatientProfile {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-      private Long id;
-      @Column(unique=true)
-      private String patientId;
-      private String email;
-      private String password;
-      private Date createAt;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique=true)
+    private String patientId;
+    private String fullName;
+    private int age;
+    @Column(unique = true)
+    private String email;
+    private String surgeryType;
+    private Boolean active;
+    private LocalDateTime createdAt;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getPatientId() {
+        return patientId;
+    }
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getSurgeryType() {
+        return surgeryType;
+    }
+    public void setSurgeryType(String surgeryType) {
+        this.surgeryType = surgeryType;
+    }
+    public Boolean getActive() {
+        return active;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public PatientProfile(Long id,@Column (unique=true) String patientId, String fullName, int age,@Column(unique=true) String email, String surgeryType,
+            Boolean active, LocalDateTime createdAt) {
+        this.id = id;
+        this.patientId = patientId;
+        this.fullName = fullName;
+        this.age = age;
+        this.email = email;
+        this.surgeryType = surgeryType;
+        this.active = active;
+        this.createdAt = createdAt;
+    }
+    public PatientProfile() {
+    }
 
 }
