442917dc4e12ca1bccba1f205ff835ffed6b02c7
bala-amypo <bala@amypo.in>
2025-12-18 17:03:58 +0000

    User commit

--- src/main/java/com/example/demo/model/PatientProfile.java
+++ src/main/java/com/example/demo/model/PatientProfile.java
@@ -1,3 +1,12 @@
 package com.example.demo.PatientProfile;
 
-public class 
+public class PatientProfile{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+      private Long id;
+      private String patientId;
+      private String email;
+      private String password;
+      private Date createAt;
+
+}
