9bdf0823714fa243cd9ebd6af2e8f05baf94565d
bala-amypo <bala@amypo.in>
2025-12-18 18:51:58 +0000

    User commit

--- src/main/java/com/example/demo/repository/RecoveryCurveProfileRepository.java
+++ src/main/java/com/example/demo/repository/RecoveryCurveProfileRepository.java
@@ -1,1 +1,14 @@
 package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.RecoveryCurveProfile;
+
+
+    @Repository
+public interface  PatientProfileRepository extends JpaRepository <RecoveryCurveProfile,Long>{
+  
+}
+
+
