e1b0547a4cee6fc7dbf56f0d49e07a059aa5b172
bala-amypo <bala@amypo.in>
2025-12-18 18:12:59 +0000

    User commit

--- src/main/java/com/example/demo/model/RecoveryCurveProfile.java
+++ src/main/java/com/example/demo/model/RecoveryCurveProfile.java
@@ -1,2 +1,65 @@
-package com.example.demo.RecoveryCurveProfile;
+package com.example.demo.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class RecoveryCurveProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String surgeryType;
+    private int dayNumber;
+    private int expectedPainLevel;
+    private int expectedMobilityLevel;
+    private int expectedFatigueLevel;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getSurgeryType() {
+        return surgeryType;
+    }
+    public void setSurgeryType(String surgeryType) {
+        this.surgeryType = surgeryType;
+    }
+    public int getDayNumber() {
+        return dayNumber;
+    }
+    public void setDayNumber(int dayNumber) {
+        this.dayNumber = dayNumber;
+    }
+    public int getExpectedPainLevel() {
+        return expectedPainLevel;
+    }
+    public void setExpectedPainLevel(int expectedPainLevel) {
+        this.expectedPainLevel = expectedPainLevel;
+    }
+    public int getExpectedMobilityLevel() {
+        return expectedMobilityLevel;
+    }
+    public void setExpectedMobilityLevel(int expectedMobilityLevel) {
+        this.expectedMobilityLevel = expectedMobilityLevel;
+    }
+    public int getExpectedFatigueLevel() {
+        return expectedFatigueLevel;
+    }
+    public void setExpectedFatigueLevel(int expectedFatigueLevel) {
+        this.expectedFatigueLevel = expectedFatigueLevel;
+    }
+    public RecoveryCurveProfile(Long id, String surgeryType, int dayNumber, int expectedPainLevel,
+            int expectedMobilityLevel, int expectedFatigueLevel) {
+        this.id = id;
+        this.surgeryType = surgeryType;
+        this.dayNumber = dayNumber;
+        this.expectedPainLevel = expectedPainLevel;
+        this.expectedMobilityLevel = expectedMobilityLevel;
+        this.expectedFatigueLevel = expectedFatigueLevel;
+    }
+    public RecoveryCurveProfile() {
+    }
+    
