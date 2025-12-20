16de79887687bc44d345874b880974bed6475352
bala-amypo <bala@amypo.in>
2025-12-20 15:26:50 +0000

    User commit

--- src/main/java/com/example/demo/service/DeviationRuleService.java
+++ src/main/java/com/example/demo/service/DeviationRuleService.java
@@ -1,1 +1,12 @@
-// package com.example.demo.service;
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.model.DeviationRule;
+
+public interface DeviationRuleService{
+DeviationRule createCurveEntry(DeviationRule curve);
+List<DeviationRule> getCurveForSurgery(String surgeryType);
+List<DeviationRule> getAllCurves();
+}
+
