6c4aa30ee47041efaba8917a7765e9ea60edda35
bala-amypo <bala@amypo.in>
2025-12-20 11:47:16 +0000

    User commit

--- src/main/java/com/example/demo/controller/RecoveryCurveController.java
+++ src/main/java/com/example/demo/controller/RecoveryCurveController.java
@@ -4,14 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.PatientProfile;
-import com.example.demo.service.PatientProfileService;
+import com.example.demo.model.RecoveryCurveProfile;
+import com.example.demo.service.RecoveryCurveService;
 
 @RestController
 public class PatientProfileController {
