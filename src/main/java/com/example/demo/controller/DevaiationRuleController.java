bb073e1b05e88e233cb6ab1d7664487d4e9c2089
bala-amypo <bala@amypo.in>
2025-12-20 15:56:51 +0000

    User commit

--- src/main/java/com/example/demo/controller/DevaiationRuleController.java
+++ src/main/java/com/example/demo/controller/DevaiationRuleController.java
@@ -9,23 +9,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.RecoveryCurveProfile;
-import com.example.demo.service.RecoveryCurveService;
+import com.example.demo.model.DeviationRule;
+import com.example.demo.service.DeviationRuleService;
 
 @RestController
-public class RecoveryCurveController {
-    @Autowired RecoveryCurveService RCS;
+public class DeviationRuleController {
+    @Autowired DeviationRuleService DRS;
     @PostMapping("/POSTMAP")
-    public RecoveryCurveProfile sendcreateCurveEntry(@RequestBody RecoveryCurveProfile entry){
-        return RCS.createCurveEntry(entry);
+    public DeviationRule sendcreateCurveEntry(@RequestBody DeviationRule entry){
+        return DRS.createCurveEntry(entry);
     }
     @GetMapping("/GETMAP/{id}")
-   public List<RecoveryCurveProfile> getgetCurveForSurgery(@PathVariable String surgeryType){
-      return RCS.getCurveForSurgery(surgeryType);
+   public List<RDeviationRule> getgetCurveForSurgery(@PathVariable String surgeryType){
+      return DRS.getCurveForSurgery(surgeryType);
    }
    @GetMapping("/GETMAP")
-   public List<RecoveryCurveProfile>getgetAllCurves(){
-    return RCS.getAllCurves();
+   public List<DeviationRule>getgetAllCurves(){
+    return DRS.getAllCurves();
    }
 
    
