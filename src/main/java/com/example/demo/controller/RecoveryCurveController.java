af7eb73cf159bf945d08411784274e7bcdee9c0c
bala-amypo <bala@amypo.in>
2025-12-20 11:41:16 +0000

    User commit

--- src/main/java/com/example/demo/controller/RecoveryCurveController.java
+++ src/main/java/com/example/demo/controller/RecoveryCurveController.java
@@ -1,38 +1,38 @@
-// package com.example.demo.controller;
+package com.example.demo.controller;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.PutMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-// import com.example.demo.model.PatientProfile;
-// import com.example.demo.service.PatientProfileService;
+import com.example.demo.model.PatientProfile;
+import com.example.demo.service.PatientProfileService;
 
-// @RestController
-// public class PatientProfileController {
-//     @Autowired PatientProfileService PPS;
-//     @PostMapping("/POST")
-//     public PatientProfile sendcreatePatient(@RequestBody PatientProfile patient){
-//         return PPS.createPatient(patient);
-//     }
-//     @GetMapping("/GET/{id}")
-//    public PatientProfile getgetPatientById(@PathVariable Long id){
-//      return PPS.getPatientById(id);
-//    }
-//    @GetMapping("/GET")
-//    public List<PatientProfile>getgetAllPatients(){
-//     return PPS.getAllPatients();
-//    }
+@RestController
+public class PatientProfileController {
+    @Autowired PatientProfileService PPS;
+    @PostMapping("/POST")
+    public PatientProfile sendcreatePatient(@RequestBody PatientProfile patient){
+        return PPS.createPatient(patient);
+    }
+    @GetMapping("/GET/{id}")
+   public PatientProfile getgetPatientById(@PathVariable Long id){
+     return PPS.getPatientById(id);
+   }
+   @GetMapping("/GET")
+   public List<PatientProfile>getgetAllPatients(){
+    return PPS.getAllPatients();
+   }
 
-//    @PutMapping("/PUT/{id}/status")
-//    public PatientProfile putupdatePatientStatus(@PathVariable Long id,@RequestBody Boolean active){
-//     return PPS.updatePatientStatus(id,active);
-//    }
+   @PutMapping("/PUT/{id}/status")
+   public PatientProfile putupdatePatientStatus(@PathVariable Long id,@RequestBody Boolean active){
+    return PPS.updatePatientStatus(id,active);
+   }
 
 
-// }
+}
