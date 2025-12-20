033a3318a86bad0b391887352a2c0afde1ccbda1
bala-amypo <bala@amypo.in>
2025-12-20 15:53:51 +0000

    User commit

--- src/main/java/com/example/demo/controller/DevaiationRuleController.java
+++ src/main/java/com/example/demo/controller/DevaiationRuleController.java
@@ -1,1 +1,32 @@
-// package com.example.demo.controller;
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.RecoveryCurveProfile;
+import com.example.demo.service.RecoveryCurveService;
+
+@RestController
+public class RecoveryCurveController {
+    @Autowired RecoveryCurveService RCS;
+    @PostMapping("/POSTMAP")
+    public RecoveryCurveProfile sendcreateCurveEntry(@RequestBody RecoveryCurveProfile entry){
+        return RCS.createCurveEntry(entry);
+    }
+    @GetMapping("/GETMAP/{id}")
+   public List<RecoveryCurveProfile> getgetCurveForSurgery(@PathVariable String surgeryType){
+      return RCS.getCurveForSurgery(surgeryType);
+   }
+   @GetMapping("/GETMAP")
+   public List<RecoveryCurveProfile>getgetAllCurves(){
+    return RCS.getAllCurves();
+   }
+
+   
+}
