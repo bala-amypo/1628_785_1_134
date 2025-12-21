bec1fb4394a625900764c02415e1066bc51db57c
bala-amypo <bala@amypo.in>
2025-12-21 05:34:51 +0000

    User commit

--- src/main/java/com/example/demo/model/DailySymptomLog.java
+++ src/main/java/com/example/demo/model/DailySymptomLog.java
@@ -1,1 +1,29 @@
-// package com.example.demo.DailySymptomLog;
+package com.example.demo.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class PatientProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String patientId;
+    private String fullName;
+    private int age;
+    private String email;
+    private String surgeryType;
+    private Boolean active;
+    private LocalDateTime createdAt;
+    
+}
