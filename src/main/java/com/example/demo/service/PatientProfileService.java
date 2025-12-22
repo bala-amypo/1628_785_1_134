3cadf97fe17175007de2081a6bfdf69082e5454f
bala-amypo <bala@amypo.in>
2025-12-22 09:23:05 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/PatientProfileService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.model.PatientProfile;
+
+public interface PatientProfileService {
+    PatientProfile createPatient (PatientProfile patient);
+    PatientProfile getPatientById(Long id);
+    List<PatientProfile>getAllPatients();
+    PatientProfile updatePatientStatus(Long id,Boolean active);
+}
+
