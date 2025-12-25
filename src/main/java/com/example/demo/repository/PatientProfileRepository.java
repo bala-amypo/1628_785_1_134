a4dcbb4d5e7d4f94f2136d23400ff32070e09c41
bala-amypo <bala@amypo.in>
2025-12-25 14:04:36 +0000

    User commit

--- src/main/java/com/example/demo/repository/PatientProfileRepository.java
+++ src/main/java/com/example/demo/repository/PatientProfileRepository.java
@@ -1,16 +1,13 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.PatientProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.model.PatientProfile;
+import java.util.Optional;
 
-
-    @Repository
-public interface  PatientProfileRepository extends JpaRepository <PatientProfile ,Long>{
-    boolean existsByEmail(String email);
-boolean existsByPatientId(String patientId);
-
+@Repository
+public interface PatientProfileRepository extends JpaRepository<PatientProfile, Long> {
+    Optional<PatientProfile> findByEmail(String email);
+    Optional<PatientProfile> findByPatientId(String patientId);
 }
-
-
--- src/main/java/com/example/demo/repository/RecoveryCurveProfileRepository.java
+++ src/main/java/com/example/demo/repository/RecoveryCurveProfileRepository.java
@@ -1,15 +1,12 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.RecoveryCurveProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.model.RecoveryCurveProfile;
 import java.util.List;
 
-
-    @Repository
-public interface  RecoveryCurveProfileRepository extends JpaRepository <RecoveryCurveProfile,Long>{
-   List<RecoveryCurveProfile> findBySurgeryTypeContaining(String value);
+@Repository
+public interface RecoveryCurveProfileRepository extends JpaRepository<RecoveryCurveProfile, Long> {
+    List<RecoveryCurveProfile> findBySurgeryType(String surgeryType);
 }
-
-
