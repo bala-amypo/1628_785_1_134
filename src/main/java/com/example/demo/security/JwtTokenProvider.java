e11358d749a47d633b1296a14cee5b9646a360b0
bala-amypo <bala@amypo.in>
2025-12-26 08:20:15 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -1,8 +1,11 @@
 package com.example.demo.security;
 
 import com.example.demo.model.AppUser;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JwtTokenProvider {
+
     public String generateToken(AppUser user) {
         return "mock-jwt";
     }
