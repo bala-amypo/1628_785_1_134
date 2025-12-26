980b95ceed7bb009ad848f6c3952d3a682139780
bala-amypo <bala@amypo.in>
2025-12-26 13:15:03 +0000

    User commit

--- src/main/java/com/example/demo/model/AppUser.java
+++ src/main/java/com/example/demo/model/AppUser.java
@@ -1,4 +1,32 @@
-package com.example.demo.model;
+// package com.example.demo.model;
+
+// import jakarta.persistence.*;
+// import lombok.AllArgsConstructor;
+// import lombok.Builder;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
+
+// @Data
+// @NoArgsConstructor
+// @AllArgsConstructor
+// @Builder
+// @Entity
+// public class AppUser {
+
+//     @Id
+//         @GeneratedValue(strategy = GenerationType.IDENTITY)
+//             private Long id;
+
+//                 private String email;
+
+//                     private String password;
+
+//                         private String fullName;
+
+//                             @Enumerated(EnumType.STRING)
+//                                 private UserRole role;
+//                                 }
+                                package com.example.demo.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
